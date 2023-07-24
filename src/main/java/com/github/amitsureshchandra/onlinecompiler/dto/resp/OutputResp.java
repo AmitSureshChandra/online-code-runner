b0b7d7b3f2673a656111cddb648b487e1df134de
Amit <akumar00029@gmail.com>
2023-07-24 08:24:06 +0530

    exception added

--- src/main/java/com/github/amitsureshchandra/onlinecompiler/dto/resp/OutputResp.java
+++ src/main/java/com/github/amitsureshchandra/onlinecompiler/dto/resp/OutputResp.java
@@ -3,10 +3,12 @@ package com.github.amitsureshchandra.onlinecompiler.dto.resp;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class OutputResp {
     private String output;
     private String error;
--- src/main/java/com/github/amitsureshchandra/onlinecompiler/service/core/RunnerService.java
+++ src/main/java/com/github/amitsureshchandra/onlinecompiler/service/core/RunnerService.java
@@ -2,13 +2,16 @@ package com.github.amitsureshchandra.onlinecompiler.service.core;
 
 import com.github.amitsureshchandra.onlinecompiler.dto.CodeReqDto;
 import com.github.amitsureshchandra.onlinecompiler.dto.resp.OutputResp;
+import com.github.amitsureshchandra.onlinecompiler.exception.ValidationException;
 import com.github.amitsureshchandra.onlinecompiler.service.lang.GoLangService;
 import com.github.amitsureshchandra.onlinecompiler.service.lang.JavaLangService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 
 @Service
+@Slf4j
 public class RunnerService {
 
     final JavaLangService javaLangService;
@@ -20,13 +23,21 @@ public class RunnerService {
     }
 
     public OutputResp run(CodeReqDto dto) throws IOException, InterruptedException {
+        OutputResp outputResp = runPrivate(dto);
+        if(outputResp.getExitCode() == 0) return outputResp;
+        log.error(outputResp.toString());
+        throw new RuntimeException("Server Error");
+    }
+
+    private OutputResp runPrivate(CodeReqDto dto) throws IOException, InterruptedException {
         switch (dto.getCompiler()) {
             case "jdk8":
             case "jdk20":
                 return javaLangService.run(dto);
             case "golang12":
                 return goLangService.run(dto);
+            default:
+                throw  new ValidationException("compiler not found");
         }
-        return new OutputResp(null, "compiler not found", -1);
     }
 }
