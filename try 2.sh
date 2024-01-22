bb733202c072e65a8dd2bf4e55e5c0a929a237d3
Amit <akumar00029@gmail.com>
2024-01-22 08:15:56 +0530

    removed

--- try 2.sh
+++
@@ -1,9 +0,0 @@
-
-
-curl --location 'http://0.0.0.0:8090/api/v1/run' \
---header 'Content-Type: application/json' \
---data '{
-    "code" : "public class Solution {public static void main(String[] args) {System.out.println(\"Hello World3\");}}",
-    "compiler" : "jdk8",
-    "input": ""
-}'
